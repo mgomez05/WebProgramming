870099bd6e5f5560644fab40cbd06958fe739716
Russ Gomez <gomezruss7@gmail.com>
2018-06-14 22:24:34 -0400

    Added style for individual messages

--- messages/lab.js
+++ messages/lab.js
@@ -20,7 +20,7 @@ function parse()
                       var message = jsonParsed[i].content;
                       var name    = jsonParsed[i].username;
                       
-                      messages.innerHTML += "<p>" + message + " <span class='author'>" + name + "</span>" + "</p>";
+                      messages.innerHTML += "<p class='message'>" + message + " <span class='author'>" + name + "</span>" + "</p>";
             }
         
             //messages.innerHTML = xhttp.responseText;
